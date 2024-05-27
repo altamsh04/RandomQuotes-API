bc8b67794e53fe917cdb103dbd82fe6c408a1caf
altamsh <bairagdaraltamsh@gmail.com>
2024-05-28 00:52:24 +0530

    Modified UI and Logic

--- api/index.js
+++ api/index.js
@@ -40,8 +40,10 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'RandomQuotesAPI.html'));
+    res.sendFile(path.join(__dirname, 'public', 'RandomQuotesAPI.html'));
 });
 
 app.get("/api/random", (req, res) => {
---
+++ api/public/script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+const text = "Random Quotes API!";
+const delay = 100;
+const element = document.getElementById('typing-heading');
+
+function typeWriter(text, i) {
+    if (i < text.length) {
+        element.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(() => {
+            typeWriter(text, i);
+        }, delay);
+    } else {
+        setTimeout(() => {
+            element.innerHTML = '';
+            typeWriter(text, 0);
+        }, 3000);
+    }
+}
+typeWriter(text, 0);
+
+function copyToClipboard() {
+    const quoteText = document.getElementById('quoteLink').innerText;
+    navigator.clipboard.writeText(quoteText).then(() => {
+        alert('Quote API link copied to clipboard');
+    }).catch(err => {
+        console.error('Failed to copy quote API link:', err);
+    });
+}
+
+function get_random_quote() {
+    fetch('https://random-quotes-freeapi.vercel.app/api/random')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("quote").textContent = data.quote; 
+        document.getElementById("quoteAuthor").textContent = "- " + data.author;
+    })
+    .catch(error => {
+        console.error('Error fetching quote:', error);
+    });
+}
+
+window.onload = get_random_quote;
+});
